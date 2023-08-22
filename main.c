621ba6dbf1ab93e2732e9454f2d2c01c6e5bf68a
cecilia-morais <142345484+cecilia-morais@users.noreply.github.com>
2023-08-21 21:08:10 -0300

    Create main.c
    Criando tela de informação

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main(void)
+{
+	   sobre();	
+	   return 0;
+}
+
+void sobre(){
+    printf("            Universidade Federal do Rio Grande do Norte\n          ");
+    printf("                Centro de Ensino Superior do Seridó\n  "            );
+    printf("                Disciplina DCT1106 -- Programação\n"                );
+    printf("                      Projeto Cell Service\n  "                     );
+    printf("           Esse projeto esta sendo desenvolvido para simular \n "   );
+    printf("                uma loja para conserto de celular.\n"               );
+    printf("                 Desenvolvido por: Cecilia Morais\n"                );
+}
