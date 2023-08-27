ff9bd6052f6225078c8a9cbfb5eb991427c32c49
cecilia-morais <142345484+cecilia-morais@users.noreply.github.com>
2023-08-26 22:34:14 -0300

    Inclusão de menu principal

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+void menu_principal(void);
 void sobre(void);
 
 int main(void)
 {
-	   sobre();	
+    menu_principal();
+    
+	sobre();	
 	   return 0;
 }
 
@@ -15,7 +17,20 @@ void sobre(){
     printf("                Centro de Ensino Superior do Seridó\n  "            );
     printf("                Disciplina DCT1106 -- Programação\n"                );
     printf("                      Projeto Cell Service\n  "                     );
-    printf("           Esse projeto esta sendo desenvolvido para simular \n "   );
+    printf("           Esse projeto está sendo desenvolvido para simular \n "   );
     printf("                uma loja para conserto de celular.\n"               );
     printf("                 Desenvolvido por: Cecilia Morais\n"                );
+}
+
+void menu_principal(){
+    system("clear || cls");
+    printf("*********************************************************************\n");
+    printf("                           MENU PRINCIPAL                            \n");
+    printf("*********************************************************************\n");
+    printf("1 - Cadastro de celular \n");
+    printf("2 - Clientes \n");
+    printf("3 - Estoques de peças\n");
+    printf("4 - Relatórios\n");
+    printf("5 - Sobre o programa\n");
+    printf("6 - Encerrar o programa\n");
 }
