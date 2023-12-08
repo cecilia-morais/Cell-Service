196e50cc0cb1c7420a679252bb1d4075da81b529
cecília-morais <cecilia.morais.706@ufrn.edu.br>
2023-12-08 17:30:24 -0300

    adicionando tela de incialização

--- main.c
+++ main.c
@@ -4,25 +4,26 @@
 #include "modulos/relatorios.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 // Assinaturas de fun��es:
 char menu_principal(void);
+void tela_inicial(void);
+void tela_carregamento(void);
 
+int main(void)
+{
+  tela_carregamento();
 
-
-int main(void) {
-  Clientes clientes[1000];
-  int qnt_clientes = 0;
-  Celulares celulares[2000];
-  int qnt_celulares = 0;
-  // Pecas pecas[1000];
-  // int qnt_pecas = 0;
   char op;
-  do {
+  do
+  {
+    tela_inicial();
     system("clear || cls");
     op = menu_principal();
 
-    switch (op) {
+    switch (op)
+    {
     case '1':
       sw_cell();
       break;
@@ -44,9 +45,12 @@ int main(void) {
       break;
 
     default:
-      if (op = '0'){
+      if (op = '0')
+      {
         printf("Encerrando...\n");
-      }else{
+      }
+      else
+      {
         printf("Opção inválida\n");
       }
       break;
@@ -57,7 +61,8 @@ int main(void) {
   return 0;
 }
 
-char menu_principal() {
+char menu_principal()
+{
   system("clear || cls");
   char menu;
   printf("*********************************************************************"
@@ -82,4 +87,60 @@ char menu_principal() {
   getchar();
 
   return menu;
+}
+
+void tela_inicial()
+{ // Função feita com ajuda do ChatGPT e aprimorada por Cecilia Morais
+  system("clear || cls");
+  char *cores[] = {"\033[0;31m", "\033[0;32m", "\033[0;34m", "\033[0;35m", "\033[0;36m"};
+  int num_cores = sizeof(cores) / sizeof(cores[0]);
+
+  int i, j;
+  for (j = 0; j < 3; j++)
+  { // Altere o número de iterações conforme necessário
+
+    for (i = 0; i < num_cores; i++)
+    {
+      printf("%s", cores[i]);
+      printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+        "        ______________                                                           \n"
+        "       |     ====     |                                                          \n"
+        "       | ____________ |          ,,---.     ,---.                 o              \n"
+        "       ||            ||         ||          `---.,---.,--. .    ,.,---.,---.     \n"
+        "       ||            ||         ||              ||---'|     \\ / ||    |---'     \n"
+        "       ||            ||          ``---' o   `---'`---'`      V   ``---'`---'     \n"
+        "       ||            ||               __________________________________________ \n"
+        "       ||            ||                    A sua loja de conserto de celulares!! \n"
+        "       ||            ||                                                          \n"
+        "       ||____________||                                                          \n"
+        "       |       o      |                                                          \n"
+        "       |______________|                                                          \n"
+        "                                                                                 \n"
+        "     >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+      printf("\033[0m"); // Código de escape para restaurar a cor padrão
+      // Aguarde um curto período para criar o efeito de piscar
+      usleep(500000);
+      system("clear || cls"); // Limpar a tela
+    }
+  }
+}
+
+
+
+void tela_carregamento()
+{
+  system("clear || cls");
+  printf("                           INICIANDO..");
+
+  int i;
+  for (i = 0; i < 3; i++)
+  {
+    printf(".");
+    fflush(stdout);  // Limpar o buffer de saída para garantir que os pontos apareçam imediatamente
+    usleep(500000);  // Aguardar 500ms entre cada ponto
+    printf("\b \b"); // Apagar o ponto anterior usando caracteres de retorno (\b) e espaço em branco
+    fflush(stdout);  // Limpar novamente o buffer de saída
+    usleep(500000);  // Aguardar mais 500ms antes do próximo ponto
+  }
+  printf("\n");
 }
